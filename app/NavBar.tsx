190abdd219e801fe3fb32401b92efc7b514b6748
Rmaki27 <engineermaki27@gmail.com>
2023-08-01 22:09:56 -0700

    Import Google font for logo

---
+++ app/NavBar.tsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <div>
+      <h1>Collections</h1>
+      {/* <Image
+            src=""
+            width={500}
+            height={500} />
+             alt="User profile picture"/> */}
+    </div>
+  );
+}
