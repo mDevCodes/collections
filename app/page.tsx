8b4bb3b009074545524f83850aa57c03d431aa0c
Rmaki27 <engineermaki27@gmail.com>
2023-08-18 14:22:00 -0700

    Add PR suggestions

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import SearchBar from "@/components/SearchBar";
 import React from "react";
 
 export default function Home() {
-  const [albumSearch, setAlbumSearch] = React.useState<string>("");
+  const [searchValue, setSearchValue] = React.useState<string>("");
 
   return (
     <main className="flex flex-col items-center justify-center ">
-      <SearchBar onChange={(value: string) => setAlbumSearch(value)} />
+      <SearchBar onChange={(value) => setSearchValue(value)} />
       <h1 className="font-bold text-6xl mt-10">Home Page</h1>
     </main>
   );
