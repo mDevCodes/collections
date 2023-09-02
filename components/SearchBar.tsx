0a2e8804ebdcf17a7840a84adca04506508869cc
Rmaki27 <engineermaki27@gmail.com>
2023-09-02 13:19:36 -0700

    Add userInput state

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import Icon from "./Icon";
 
 export default function SearchBar({
@@ -7,6 +8,8 @@ export default function SearchBar({
 }: {
   onClick: (value: string) => void;
 }) {
+  const [userInput, setUserInput] = useState("");
+  console.log(userInput);
   return (
     <div className="m-auto w-3/4 flex justify-center mt-10 relative">
       <Icon
@@ -15,13 +18,14 @@ export default function SearchBar({
         size="small"
       />
       <input
-        id="search-input"
+        id="search"
         type="text"
         placeholder="Search for your favorite records"
         className="w-5/6 p-3 pl-14 rounded-full text-black"
+        onChange={(e) => setUserInput(e.target.value)}
       />
       <button
-        onClick={(e) => onClick("flume")}
+        onClick={() => onClick("flume")}
         className="ml-8 p-4 border-2 rounded-full"
       >
         Search
