b88f22057ca18b0aedfcd9c95e9a278b124edda5
Rmaki27 <engineermaki27@gmail.com>
2023-09-03 13:23:12 -0700

    Update album schema, render album images and data

--- app/utils/discogs.schemas.ts
+++ app/utils/discogs.schemas.ts
@@ -1,8 +1,10 @@
 import z from "zod";
 
 const AlbumSchema =  z.object({
+  cover_image:z.string(),
+  title: z.string().optional(),
   year: z.string().optional(),
-  id: z.number().optional()
+  id: z.number().optional(),
 })
 
 export type Album = z.infer<typeof AlbumSchema>
--- components/Result.tsx
+++ components/Result.tsx
@@ -1,9 +1,21 @@
 import { Album } from "@/app/utils/discogs.schemas";
+import Image from "next/image";
 
 export default function Result({ album }: { album: Album }) {
   return (
-    <>
-      <h3>Year: {album.year}</h3>
-    </>
+    <div className="flex">
+      <Image
+        src={album.cover_image}
+        alt={`${album.title} cover image`}
+        width={200}
+        height={200}
+      />
+      <h2>Image {album.cover_image ? "Yes" : "No"}</h2>
+      <div className="flex flex-column">
+        <h3>Year: {album.year}</h3>
+        <h3>Title: {album.title}</h3>
+        <h3>Title: {album.title}</h3>
+      </div>
+    </div>
   );
 }
