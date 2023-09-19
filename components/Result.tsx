866cf988d14f866aa148923e48553ca7a153bd74
Rmaki27 <engineermaki27@gmail.com>
2023-09-18 22:05:22 -0700

    Conditionally render home icon for any cover image result that ends in .gif

--- components/Result.tsx
+++ components/Result.tsx
@@ -1,15 +1,20 @@
 import { Album } from "@/app/utils/discogs.schemas";
 import Image from "next/image";
+import Icon from "./Icon";
 
 export default function Result({ album }: { album: Album }) {
   return (
     <div className="flex mt-16">
-      <Image
-        src={album.cover_image}
-        alt={`${album.title} cover image`}
-        width={200}
-        height={200}
-      />
+      {album.cover_image.endsWith(".gif") ? (
+        <Icon type="home" size="medium" />
+      ) : (
+        <Image
+          src={album.cover_image}
+          alt={`${album.title} cover image`}
+          width={200}
+          height={200}
+        />
+      )}
       <div className="ml-14">
         <h3 className="text-lg font-bold">Title: {album.title}</h3>
         <h3>Year: {album.year ? album.year : "-"}</h3>
