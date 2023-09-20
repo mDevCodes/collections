80237684d8f82e646a657ee8a9d15d467d4217ca
Rmaki27 <engineermaki27@gmail.com>
2023-09-19 18:49:55 -0700

    Clean up zod schemas

--- app/utils/discogs.schemas.ts
+++ app/utils/discogs.schemas.ts
@@ -1,28 +1,16 @@
 import z from "zod";
 
-const AlbumSchemaMod =  z.object({
+const AlbumSchema = z.object({
   id: z.number(),
-  cover_image:z.string(),
+  cover_image: z.string(),
   title: z.string(),
   year: z.string().optional(),
-})
-
-const AlbumSchema =  z.object({
-  id: z.number(),
-  cover_image:z.string(),
-  title: z.string(),
-  year: z.string().optional(),
-})
-
-
-export type Album = z.infer<typeof AlbumSchema>
-export type AlbumMod = z.infer<typeof AlbumSchema>
-
-
-const SearchResponseSchema = z.object({
-  results: z.array(
-   AlbumSchema
-  ),
 });
 
-export {SearchResponseSchema};
+export type Album = z.infer<typeof AlbumSchema>;
+
+const SearchResponseSchema = z.object({
+  results: z.array(AlbumSchema),
+});
+
+export { SearchResponseSchema };
