0cbbacb502358d04fe4325bce45b5b2f2df15d8d
Rmaki27 <engineermaki27@gmail.com>
2023-09-30 16:11:55 -0700

    Adjust Result styles

--- components/Result.tsx
+++ components/Result.tsx
@@ -4,7 +4,7 @@ import { Album } from "@/schemas/collections.schemas";
 
 export default function Result({ album }: { album: Album }) {
   return (
-    <div className="flex p-24 border-bottom-solid border-b border-gray-800">
+    <div className="flex gap-6 px-8 py-24  border-bottom-solid border-b border-gray-800">
       {album.coverImage.endsWith(".gif") ? (
         <div className="w-[200px] h-[200px] p-12 rounded-sm bg-gray-800">
           <Icon
@@ -22,7 +22,7 @@ export default function Result({ album }: { album: Album }) {
           height={200}
         />
       )}
-      <div className="ml-14">
+      <div>
         <p className="text-xl font-bold">{album.albumTitle}</p>
         <p className="text-lg">{album.artist}</p>
         <p className="text-lg">{album.year ? album.year : "-"}</p>
