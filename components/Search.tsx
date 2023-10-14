3ba56c22c7d02969e6a940d0cc8d5f1480cc3488
Rmaki27 <engineermaki27@gmail.com>
2023-10-13 19:40:45 -0700

    Add debounce function to client search value state

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,19 +1,26 @@
 "use client";
-import React from "react";
+import { useState, useEffect } from "react";
+import { debounce } from "lodash";
 import SearchBar from "./SearchBar";
 import SearchResult from "./SearchResult";
 
 export default function Search() {
-  const [searchValue, setSearchValue] = React.useState<string>("");
+  const [clientSearchValue, setClientSearchValue] = useState<string>("");
+  const [serverSearchValue, setServerSearchValue] = useState<string>("");
+
+  useEffect(
+    () => debounce(() => setServerSearchValue(clientSearchValue), 700),
+    [clientSearchValue]
+  );
 
   return (
     <>
       <SearchBar
-        searchValue={searchValue}
-        onSearch={(value) => setSearchValue(value)}
-        onClear={() => setSearchValue("")}
+        searchValue={clientSearchValue}
+        onSearch={(value) => setClientSearchValue(value)}
+        onClear={() => setClientSearchValue("")}
       />
-      <SearchResult searchValue={searchValue} />
+      <SearchResult searchValue={serverSearchValue} />
     </>
   );
 }
