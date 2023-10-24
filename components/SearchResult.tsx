e287f54eee07a1b1b3d593a593870df61028d059
Rmaki27 <engineermaki27@gmail.com>
2023-10-23 20:05:03 -0700

    Add loading spinner

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import Result from "./Result";
 import { Album, SearchResponseSchema } from "@/schemas/collections.schemas";
 import ResultLoading from "./ResultLoading";
+import { PulseLoader } from "react-spinners";
+import { CSSProperties } from "react";
 
 export default function SearchResult({ searchValue }: { searchValue: string }) {
   const { data, isError, isLoading } = useQuery({
@@ -19,9 +21,18 @@ export default function SearchResult({ searchValue }: { searchValue: string }) {
   });
 
   if (isLoading) {
+    const override: CSSProperties = {
+      marginBottom: "12px",
+    };
     const loadingUiArray = Array(10).fill(null);
     return (
       <>
+        <PulseLoader
+          color="white"
+          loading={isLoading}
+          size={10}
+          cssOverride={override}
+        />
         {loadingUiArray.map((_, index) => (
           <ResultLoading key={index} />
         ))}
@@ -35,6 +46,7 @@ export default function SearchResult({ searchValue }: { searchValue: string }) {
 
   return (
     <>
+      <div className="w-full lg:h-6 lg:mb-3"></div>
       {data?.results.map((album: Album) => (
         <Result key={album.id} album={album} />
       ))}
