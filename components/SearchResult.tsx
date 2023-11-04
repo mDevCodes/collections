50bf8a83f6b9f405b97a3c0f4d779f33d9fcd9d6
Rmaki27 <engineermaki27@gmail.com>
2023-11-03 20:29:00 -0700

    Update SearchResult to utilize useInfiniteQuery

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -1,24 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import Result from "./Result";
 import { Album, SearchResponseSchema } from "@/schemas/collections.schemas";
 import ResultLoading from "./ResultLoading";
 
 export default function SearchResult({ searchValue }: { searchValue: string }) {
-  const { data, isError, isLoading } = useQuery({
+  const { data, error, isFetching } = useInfiniteQuery({
     queryKey: ["search", searchValue],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       const params = new URLSearchParams({
         search: searchValue,
+        page: String(pageParam),
       });
       const res = await fetch("/api/search?" + params);
       const data = await res.json();
       const parsedData = SearchResponseSchema.parse(data);
-
       return parsedData;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      return lastPageParam + 1;
     },
   });
 
-  if (isLoading) {
+  if (isFetching) {
     const loadingUiArray = Array(10).fill(null);
     return (
       <>
@@ -29,11 +33,11 @@ export default function SearchResult({ searchValue }: { searchValue: string }) {
     );
   }
 
-  if (isError) {
+  if (error) {
     return <h3>Error</h3>;
   }
 
-  if (data?.results.length === 0) {
+  if (data?.pages.length === 0) {
     return (
       <div className="flex justify-center">
         <p>No results found</p>
@@ -43,8 +47,12 @@ export default function SearchResult({ searchValue }: { searchValue: string }) {
 
   return (
     <>
-      {data?.results.map((album: Album) => (
-        <Result key={album.id} album={album} />
+      {data?.pages.map((page) => (
+        <>
+          {page.results.map((album: Album) => (
+            <Result key={album.id} album={album} />
+          ))}
+        </>
       ))}
     </>
   );
