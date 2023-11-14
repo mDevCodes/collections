a21d0cda0bb7c84feed5698811687bbfaa5e2103
Rmaki27 <engineermaki27@gmail.com>
2023-11-13 21:10:18 -0800

    Setup auth with credentials without database

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,30 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "text", placeholder: "password" },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "54",
+          name: "Makii",
+          password: "password-test",
+        };
+
+        if (
+          credentials?.username === user.name &&
+          credentials.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
--- app/api/auth/nextauth/route.ts
+++
@@ -1,2 +0,0 @@
-import nextAuth from "next-auth";
-import NextAuth from "next-auth/next";
