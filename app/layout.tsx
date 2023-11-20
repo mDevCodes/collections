82e14719b2de025c826fc55a72772e4a995bb6fa
Rmaki27 <engineermaki27@gmail.com>
2023-11-19 23:04:37 -0800

    Move SessionProvider to Providers component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "../components/NavBar";
 import Providers from "../components/Providers";
-import { getServerSession } from "next-auth";
-import SessionProvider from "@/components/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +16,13 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>
+        <Providers>
           <NavBar />
-          <Providers>{children}</Providers>
-        </SessionProvider>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,40 +6,37 @@ import { useSession } from "next-auth/react";
 
 const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 const inter = Inter({ weight: "400", subsets: ["latin"] });
-import { SessionProvider } from "next-auth/react";
 
 export default function NavBar() {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   return (
-    <SessionProvider>
-      <header
-        className={`flex justify-between items-center border-b border-gray-800 p-4 ${lobster.className}`}
-      >
-        <Link href="/">
-          <h1 className="text-2xl">Collections</h1>
-        </Link>
+    <header
+      className={`flex justify-between items-center border-b border-gray-800 p-4 ${lobster.className}`}
+    >
+      <Link href="/">
+        <h1 className="text-2xl">Collections</h1>
+      </Link>
 
-        {status === "authenticated" ? (
-          <div className="flex flex-row gap-3 items-center">
-            <Link href="/user-profile">
-              <Image
-                src="/images/profile-picture.jpg"
-                width={32}
-                height={32}
-                alt="User profile picture"
-                className="rounded-full"
-              />
-            </Link>
-            <Link href={"/api/auth/signout"}>
-              <button className={`${inter.className} `}>Sign Out</button>
-            </Link>
-          </div>
-        ) : (
-          <Link href="/api/auth/signin">
-            <button className={`${inter.className} `}>Sign In</button>
+      {status === "authenticated" ? (
+        <div className="flex flex-row gap-3 items-center">
+          <Link href="/user-profile">
+            <Image
+              src="/images/profile-picture.jpg"
+              width={32}
+              height={32}
+              alt="User profile picture"
+              className="rounded-full"
+            />
           </Link>
-        )}
-      </header>
-    </SessionProvider>
+          <Link href={"/api/auth/signout"}>
+            <button className={`${inter.className} `}>Sign Out</button>
+          </Link>
+        </div>
+      ) : (
+        <Link href="/api/auth/signin">
+          <button className={`${inter.className} `}>Sign In</button>
+        </Link>
+      )}
+    </header>
   );
 }
