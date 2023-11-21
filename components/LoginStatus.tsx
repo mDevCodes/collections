afac7b681df4b5d601d069f7bee568e0902cf417
Rmaki27 <engineermaki27@gmail.com>
2023-11-20 23:06:36 -0800

    Render sign in status after session is initialized

---
+++ components/LoginStatus.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+
+export default function LoginStatus() {
+  const { status } = useSession();
+
+  if (status !== "loading") {
+    if (status === "authenticated") {
+      return (
+        <div className="flex flex-row gap-3 items-center">
+          <Link href="/user-profile">
+            <Image
+              src="/images/profile-picture.jpg"
+              width={32}
+              height={32}
+              alt="User profile picture"
+              className="rounded-full"
+            />
+          </Link>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </div>
+      );
+    }
+    return <button onClick={() => signIn()}>Sign In</button>;
+  }
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,8 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { signIn, signOut } from "next-auth/react";
+import LoginStatus from "./LoginStatus";
 
 export default function NavBar() {
-  const { status } = useSession();
   return (
     <header
       className={`flex justify-between items-center border-b border-gray-800 p-4`}
@@ -13,25 +10,7 @@ export default function NavBar() {
       <Link href="/">
         <h1 className="text-2xl font-heading">Collections</h1>
       </Link>
-
-      {status === "authenticated" ? (
-        <div className="flex flex-row gap-3 items-center">
-          <Link href="/user-profile">
-            <Image
-              src="/images/profile-picture.jpg"
-              width={32}
-              height={32}
-              alt="User profile picture"
-              className="rounded-full"
-            />
-          </Link>
-          <button onClick={() => signOut()}>Sign Out</button>
-        </div>
-      ) : (
-        <Link href="/api/auth/signin">
-          <button onClick={() => signIn()}>Sign In</button>
-        </Link>
-      )}
+      <LoginStatus></LoginStatus>
     </header>
   );
 }
